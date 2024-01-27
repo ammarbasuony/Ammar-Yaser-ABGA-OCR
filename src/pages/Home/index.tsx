a79888743dd2646f322532e2ed767c9221e9a590
Ammar Yaser <ammaryaser.dev@gmail.com>
2024-01-27 02:48:36 +0200

    .

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="bg-primary py-10">
+      <div className="text-center text-white text-2xl">
+        ID & Passport <br /> <span className="font-extrabold uppercase text-4xl">Scanner</span>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
